f5d12c2579f2ce9f6f2e4adba6551f683e1709b4
MarlyssaB <marlyssa.boucher@gmail.com>
2019-10-10 18:40:30 -0500

    first commit

--- CS321_W4D1_BookAPI/Controllers/BooksController.cs
+++ CS321_W4D1_BookAPI/Controllers/BooksController.cs
@@ -22,7 +22,7 @@ namespace CS321_W4D1_BookAPI.Controllers
         {
             // TODO: convert domain models to apimodels
             var bookModels = _bookService
-                .GetAll();
+                .GetAll().ToApiModels();
 
             return Ok(bookModels);
         }
@@ -33,7 +33,7 @@ namespace CS321_W4D1_BookAPI.Controllers
         public IActionResult Get(int id)
         {
             // TODO: convert domain model to apimodel
-            var book = _bookService.Get(id);
+            var book = _bookService.Get(id).ToApiModel;
             if (book == null) return NotFound();
             return Ok(book.ToApiModel());
         }
@@ -47,7 +47,7 @@ namespace CS321_W4D1_BookAPI.Controllers
             {
                 // TODO: convert apimodel to domain model
                 // add the new book
-                _bookService.Add(newBook);
+                 _bookService.Add(newBook.ToDomainModel());
             }
             catch (System.Exception ex)
             {
@@ -62,7 +62,7 @@ namespace CS321_W4D1_BookAPI.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] BookModel updatedBook)
         {
-            var book = _bookService.Update(updatedBook);
+            var book = _bookService.Update(updatedBook.ToDomainModel());
             if (book == null) return NotFound();
             return Ok(book.ToApiModel());
         }
@@ -76,5 +76,38 @@ namespace CS321_W4D1_BookAPI.Controllers
             _bookService.Remove(book);
             return NoContent();
         }
+
+        // GET api/author/{authorId}/books
+        [HttpGet("/api/authors/{authorId}/books")]
+        public IActionResult GetBooksForAuthor(int authorId)
+        {
+            var bookModels = _bookService
+                .GetBooksForAuthor(authorId)
+                .ToApiModels();
+
+            return Ok(bookModels);
+        }
+
+        public IEnumerable<Book> GetBooksForAuthor(int authorId)
+        {
+            return _bookContext.Books
+                .Include(b => b.Publisher)
+                .Include(b => b.Author)
+                .Where(b => b.AuthorId == authorId)
+                .ToList();
+        }
+        // GET api/author/{authorId}/books
+         // NOTE that the route specified in HttpGet begins with a forward slash.
+         // This overrides the Route("/api/[controller]") specified on the BooksController
+         // class.
+         [HttpGet("/api/authors/{authorId}/books")]
+         public IActionResult GetBooksForAuthor(int authorId)
+         {
+             var bookModels = _bookService
+                 .GetBooksForAuthor(authorId)
+                 .ToApiModels();
+
+             return Ok(bookModels);
+         }
     }
 }
